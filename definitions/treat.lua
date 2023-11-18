df0d616c42e1389911aba9470d2d9d5a94432193
vicktor12 <victorsouza0@icloud.com>
2023-11-18 14:58:48 -0300

    Criado o modulo de criaturas do jogo

---
+++ definitions/treat.lua
@@ -0,0 +1,11 @@
+local treat = {}
+
+treat.name = "Treat"
+treat.description = "Uma arvore gigantesca da floresta mágica"
+treat.maxHealth = 50
+treat.health = 50
+treat.attack = 4
+treat.defense = 8
+treat.speed = 1
+
+return treat
