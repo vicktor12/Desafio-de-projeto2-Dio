19d0e3d62e9e6b462447a510204fafeeed430c12
vicktor12 <victorsouza0@icloud.com>
2023-11-18 14:58:37 -0300

    Criado o modulo com os atributos do player

---
+++ definitions/player.lua
@@ -0,0 +1,11 @@
+local player = {}
+
+player.name = "Victor Shaw"
+player.description = "Um especilista em armamentos"
+player.maxHealth = 10
+player.health = 10
+player.attack = 6
+player.defense = 4
+player.speed = 3
+
+return player
