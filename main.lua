71241f53e5afb3c66aaee2cb1864a2ae0199762a
vicktor12 <victorsouza0@icloud.com>
2023-11-18 14:59:10 -0300

    Redefinido o arquivo main, para puxar dos modulos

--- main.lua
+++ main.lua
@@ -1,18 +1,18 @@
-os.execute("chcp 65001")
----Header
-print([[
-    ========================================================
-    
-                  />_________________________________
-         [########[]_________________________________>
-                  \>
-    
-          ------------------------------------------
-                ⚔️  Simulador de Batalha  ⚔️
-    
-    =========================================================
-    
-              Pegue sua arma e venha para a luta!
-    ]])
+---Dependecies
+local utils = require("utils")
+local player = require("definitions.player")
+local treat = require("definitions.treat")
 
----Apresentar criatura
+utils.enableUTF8()
+
+---Header
+utils.printHeader()
+
+
+---Definição do jogador
+-- print(string.format("A vida do jogador é %d/%d", player.health, player.maxHealth))
+
+local boss = treat
+
+---Apresentando a criatura
+utils.printCreature(boss)
